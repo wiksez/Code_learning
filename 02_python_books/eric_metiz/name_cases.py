a7d8d7fa91367cc1c16f757f403f5a2f53980355
wiksez <moderviktor@gmail.com>
2024-12-11 18:25:26 +0100

    name cases

---
+++ 02_python_books/eric_metiz/name_cases.py
@@ -0,0 +1,15 @@
+name = "Eric"
+name2 = "harry potter"
+print("Hello " + name + ", would you like to learn some Python today?")
+print(name2.title())
+print(name2.lower())
+print(name2.upper())
+print('"The only way to do great work is to love what you do." - Steve Jobs')
+
+famous_person = "Winston Churchill"
+message = '"Success is not final, failure is not fatal: It is the courage to continue that counts." - ' + famous_person
+print(message)
+
+name3 = "  Maria    "
+print(name3)
+print("\t" + name3.lstrip() + "." + "\n\t" + name3.rstrip() + "." + "\n\t" + name3.strip() + ".")
