b3c1bea37f3eab38dd19b7d71d3054e233e62637
wiksez <moderviktor@gmail.com>
2024-12-25 12:51:44 +0100

    Block letters

---
+++ 01/letters.py
@@ -0,0 +1,21 @@
+# Display the first initial 'V'
+first_initial = """v   v
+v   v
+v   v
+v   v
+v   v
+ v v 
+  v
+"""
+print(first_initial)
+
+# Display the second initial 'k'
+second_initial = """k   k
+k  k
+k k
+kk
+k k
+k  k
+k   k
+"""
+print(second_initial)
