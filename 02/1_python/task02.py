d5e7147b7ead693ecdecc2e575dc2d9774d3c7f6
wiksez <moderviktor@gmail.com>
2024-12-26 13:56:13 +0100

    data types

---
+++ 02/1_python/task02.py
@@ -0,0 +1,8 @@
+num_int = 21
+num_float = 4.25
+num_str = "15"
+num_bool = True
+print(f"Variable num_int has value {num_int}")
+print(f"Variable num_float has value {num_float}")
+print(f"Variable num_str has value {num_str}")
+print(f"Variable num_bool has value {num_bool}")
